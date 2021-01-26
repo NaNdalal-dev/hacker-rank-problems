fc614b60cf90db3ffcfac55252ea070fc70a3f4c
NaNdalal-dev <dnandalal7@gmail.com>
2021-01-26 12:49:17 +0530

    added balanced_brackets.py

---
+++ Stack/balanced_brackets.py
@@ -0,0 +1,26 @@
+'''
+Link:
+https://www.hackerrank.com/challenges/balanced-brackets/problem
+'''
+def isBalanced(s):
+	lgt = len(s)
+	if lgt%2 == 1:
+		return "NO"
+	flag = 1
+	stk = [s[0]]
+	for val in s[1:]:
+		stk.append(val)
+		if flag:
+			if (stk[-2] =="(" and stk[-1] == ")") or (stk[-2] =="{" and stk[-1] == "}") or (stk[-2] =="[" and stk[-1] == "]"):
+				stk.pop()
+				stk.pop()
+		if stk == []:
+			flag = 0
+		else:
+			flag = 1
+			
+	if stk == [] :
+		return "YES"
+	return "NO"
+
+print(isBalanced('{(([])[])[]}[]'))
