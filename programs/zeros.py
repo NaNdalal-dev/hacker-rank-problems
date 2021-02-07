9f4efa53ce74de54c7717f50f2d638d7476656e8
NaNdalal-dev <dnandalal7@gmail.com>
2021-02-07 12:55:30 +0530

    Modified zeros.py solution completed

--- programs/zeros.py
+++ programs/zeros.py
@@ -1,8 +1,17 @@
 '''
 Link:
 https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/train/python
+
+Reference Link:
+https://brilliant.org/wiki/trailing-number-of-zeros/
 '''
+
+from math import log,ceil
 def zeros(n):
-	if n <= 100:
-		return (n//5) + (n//25)
-print(zeros(100))
+	if n <= 4:
+		return 0
+	k = int(ceil(log(n,5)))
+	res = 0
+	for i in range(1,k+1):
+		res += (n//(5**i))
+	return res
