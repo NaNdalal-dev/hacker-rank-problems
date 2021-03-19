e1389ec7121b41a5e755e916bb198769425a89dd
NaNdalal-dev <dnandalal7@gmail.com>
2021-03-19 11:11:15 +0530

    Tower Builder

--- programs/tower_builder.py
+++ programs/tower_builder.py
@@ -2,13 +2,15 @@
 Link:
 https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/python
 '''
-'''def tower_builder(n):
-	for i in range(n):
-		print(' '*(n-i-1),end='')
-		
-		print('* '*(i+1),end='')
 
-
-		
-		print()
-'''
+def tower_builder(n_floors):
+	spaces = n_floors - 1
+	tower = []
+	towerStr = " "
+	stars = 1
+	for i in range(n_floors):
+		x = towerStr*spaces + '*'*(stars) + towerStr*spaces
+		tower.append(x)
+		spaces -= 1
+		stars += 2
+	return tower
